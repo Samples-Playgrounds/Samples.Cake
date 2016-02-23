e3afc4f8c6659ab5282cdd134a3be5336997012c
moljac <mcvjetko@holisticware.net>
2016-02-24 00:33:57 +0100

    mac environment

---
+++ Microsoft.DevUG.Zg-20160220/step03/mac-environment.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export PATH=/Library/Frameworks/Mono.framework/Commands/:/Applications/Xamarin\ Studio.app/Contents/MacOS/:$PATH
+export PATH="/Library/Frameworks/Mono.framework/Commands/:/Applications/Xamarin Studio.app/Contents/MacOS/:$PATH"

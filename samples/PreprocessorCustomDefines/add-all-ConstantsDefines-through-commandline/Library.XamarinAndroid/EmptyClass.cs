b7e5aad3f885fd558e14df35f93d7a63e6983461
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-10-11 12:43:26 +0200

    projects completed

---
+++ samples/PreprocessorCustomDefines/add-all-ConstantsDefines-through-commandline/Library.XamarinAndroid/EmptyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Library
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+            DoSomethingLikeInitialize();
+
+            return;
+        }
+
+        #if XAMARIN_AUTH_INTERNAL
+        public void DoSomethingLikeInitialize()
+        {
+            return;
+        }
+        #endif
+    }
+}
